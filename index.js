1bf0071cb99c6528f74982a71f009a4fcb8b3f50
Mitesh Tiwari <80743610+miteshtiwari@users.noreply.github.com>
2024-04-26 20:31:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//database connect
+
+//middlewares
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	return res.json({
+		success:true,
+		message:'Your server is up and running sparsh bro....'
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`App is running at ${PORT}`)
+})
+
